6806c5b8a32ddf02b5f604cbdca40742d76b55b5
balaprasanna <balaprasannav2009@gmail.com>
2015-08-13 22:10:07 +0800

    Sails App -now connected with MongoDB and started exposes the api for nutritionInfo, with a given query ?q=something , at the route /NutritionInfo/food?q=something

--- SailsApp/HolmuskWebScrapper/api/controllers/NutritionInfoController.js
+++ SailsApp/HolmuskWebScrapper/api/controllers/NutritionInfoController.js
@@ -4,8 +4,34 @@
  * @description :: Server-side logic for managing Nutritioninfoes
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+var url = require('url')
 
 module.exports = {
+
+	food: function (req, res) {		
+	var r = req.url;
+	console.log(r)
+	var urlInfo = url.parse(r,true)
+	console.log(urlInfo)
+	var q = urlInfo.query.q;
+	console.log(q)
 	
+		NutritionInfo.find(
+			{
+			  food_name : {
+			    'contains' : q
+			  },
+			  limit: 10
+			}
+		).exec(function (err, allTheStuff) {
+			if(!err){
+				console.log(allTheStuff)
+				res.json(allTheStuff)
+			}else
+			{
+				console.log(err)			
+			}
+		})
+	},	
 };
 
