800a48c45af529f24426e62db0b9c3d7f56128db
balaprasanna <balaprasannav2009@gmail.com>
2015-08-13 21:56:48 +0800

    Model - NutritionInfo Added

--- SailsApp/HolmuskWebScrapper/api/models/NutritionInfo.js
+++ SailsApp/HolmuskWebScrapper/api/models/NutritionInfo.js
@@ -7,7 +7,63 @@
 
 module.exports = {
 
+  tableName: 'nutritioninfos',
   attributes: {
+  	food_name: {
+  		type: 'string'
+  	},
+  	Calories:{
+  		type: 'string'	
+  	},
+  	Sodium:{
+  		type: 'string'	
+  	},
+  	'Total Fat':{
+  		type: 'string'	
+  	},
+  	Potassium:{
+  		type: 'string'	
+  	},
+  	'Total Carbs':{
+  		type: 'string'	
+  	},
+  	Polyunsaturated:{
+  		type: 'string'	
+  	},
+  	'Dietary Fiber':{
+  		type: 'string'	
+  	},
+  	Monounsaturated:{
+  		type: 'string'	
+  	},
+  	Sugars:{
+  		type: 'string'	
+  	},
+  	Trans:{
+  		type: 'string'	
+  	},
+  	Protein:{
+  		type: 'string'	
+  	},
+  	'Vitamin A':{
+  		type: 'string'	
+  	},
+  	Calcium:{
+  		type: 'string'	
+  	},
+  	'Vitamin C':{
+  		type: 'string'	
+  	},
+  	Iron:{
+  		type: 'string'	
+  	},
+
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.id;
+      delete obj.__v;
+      return obj;
+    }
 
   }
 };
