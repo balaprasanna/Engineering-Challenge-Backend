cf97fc7a23106ceca59e9c66a005f16cc6da088b
balaprasanna <balaprasannav2009@gmail.com>
2015-08-14 11:34:10 +0800

    Auto Complete Task achieved - Started working on new ui module to display the nutrition info for that Food Name

--- SailsApp/HolmuskWebScrapper/assets/js/script.js
+++ SailsApp/HolmuskWebScrapper/assets/js/script.js
@@ -6,6 +6,7 @@
 
 // });
 $(document).ready(function(){
+	
 	$("#search-box").keyup(function(){
 		if($(this).val() == ""){
 			$('#suggesstion-box').html('');
@@ -16,8 +17,7 @@ $(document).ready(function(){
 			type: "GET",
 			url: "/Food/getSuggestedFoodNames",
 			data:'q='+$(this).val(),
-			beforeSend: function(){
-				//$("#search-box").css("background","#FFF url(LoaderIcon.gif) no-repeat 165px");
+			beforeSend: function(){				
 			},
 			success: function(data){
 				
@@ -28,9 +28,7 @@ $(document).ready(function(){
 					var foodname = resultObject.food_name;
 					$("#newList").append("<li>"+ foodname +"</li>");
 				})
-
-				$("#suggesstion-box").show();			
-				//$("#search-box").css("background","#FFF");
+				$("#suggesstion-box").show();				
 			}
 			});
 		}
